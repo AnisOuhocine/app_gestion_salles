4fc8d126c69d03b24aab88b20fe1e4de5e55c74a
Anis Ouhocine <300151496@monboreal.ca>
2026-04-18 11:46:58 -0400

    Creation de la classe ServiceSalle dans service_salle

--- services/service_salle.py
+++ services/service_salle.py
@@ -1,1 +1,8 @@
 # fichier en préparation
+
+from data.dao_salle import DataSalle
+from models.salle import Salle
+
+class ServiceSalle:
+    def __init__(self):
+        self.dao_salle = DataSalle()
