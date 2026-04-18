94c9024aa314556cdcd3d3a38d872d1c9f5494ee
Anis Ouhocine <300151496@monboreal.ca>
2026-04-18 10:05:31 -0400

    tester si la classe salle et sa methode on été bien fait et le resultat est correcte

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
 # fichier en préparation
 # Etape 3 : Initialisation de l architecture en couches et installation des dependances
 
+from models.salle import Salle
+
+s1 = Salle("S1", "Salle info", "Laboratoire", 30)
+s1.afficher_infos()
