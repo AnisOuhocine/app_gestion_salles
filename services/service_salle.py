4a6d4cc94b109b92544180f4e7d6e67da199e276
Anis Ouhocine <300151496@monboreal.ca>
2026-04-18 11:52:27 -0400

    Ajout de la validation dans la methode  ajouter_salle

--- services/service_salle.py
+++ services/service_salle.py
@@ -6,3 +6,10 @@ from models.salle import Salle
 class ServiceSalle:
     def __init__(self):
         self.dao_salle = DataSalle()
+    def ajouter_salle(self,salle):
+        if not salle.code or not salle.libelle or not salle.type or not salle.capacite:
+            return False, "Tous les champs sont obligatoires"
+        if salle.capacite < 1 :
+            return False, "La capacite doit etre superieure ou egale 1"
+        self.dao_salle.insert_salle(salle)
+        return True, "Salle ajoutee avec succes"
