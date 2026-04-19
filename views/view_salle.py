ee47ed19de13e18cf4f751f86132c34972d51644
Anis Ouhocine <300151496@monboreal.ca>
2026-04-19 00:02:21 -0400

    Creation de la fenetre principale WiewSalle

--- views/view_salle.py
+++ views/view_salle.py
@@ -1,1 +1,14 @@
 # fichier en préparation
+
+import customtkinter as ctk
+from tkinter import messagebox
+from services.service_salle import ServiceSalle
+from models.salle import Salle
+
+class ViewSalle(ctk,CTk):
+    def __init__(self):
+        super().__init__()
+
+        self.service_salle = ServiceSalle()
+        self.title("Gestion des salles")
+        self.geometry ("700x400")
